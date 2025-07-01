4918c6b0058a0423d44a3c43a95e36b2947874b5
valedepact <denistvalelian@gmail.com>
2025-07-01 16:51:25 +0300

    added the visit schedule page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:care_flow/screens/visit_schedule_page.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:care_flow/screens/login_page.dart';
-import 'package:care_flow/screens/register_page.dart';
+import 'package:care_flow/screens/home_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,6 +12,10 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Care Flow',
+      initialRoute: '/',
+      routes: {
+        '/visitSchedule': (context)=> VisitSchedulePage(),
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -20,81 +23,6 @@ class MyApp extends StatelessWidget{
     );
   }
 }
-class MyHomePage extends StatefulWidget{
-  @override
-  MyHomePageState createState() {
-    return MyHomePageState();
-  }
-}
-class MyHomePageState extends State<MyHomePage> {
-  final _formKey = GlobalKey<FormState>();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final _fullNameController = TextEditingController();
-  final _registerUsernameController = TextEditingController();
-  final _registerEmailController = TextEditingController();
-  final _registerPasswordController = TextEditingController();
-  final _confirmPasswordController = TextEditingController();
-  final String _selectedRole = "Nurse"; //this variable's value won't change
-  @override
-  Widget build(BuildContext context){
-  return Scaffold(
-    body: SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          Center(
-            child: Text("Care Flow",
-            style: TextStyle(fontSize:24, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(height: 32),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(child: LoginCard()),
-              SizedBox(width: 16),
-              Expanded(child: RegisterCard()),
-            ],
-          ),
-          SizedBox(height: 24),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(onPressed: () {},
-                  child:Text("Terms and Conditions"),
-              ),
-              VerticalDivider(),
-              TextButton(
-                onPressed: () {},
-                child: Text("Privacy Policy"),
-              ),
-            ],
-          ),
-          SizedBox(height: 16),
-          Text("Or login with"),
-          SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-              icon: FaIcon(FontAwesomeIcons.google),
-                  label: Text("Google"),
-                onPressed: () {},
-              ),
-              SizedBox(width: 16),
-              ElevatedButton.icon(
-                icon: Icon(Icons.facebook),
-                label: Text("Facebook"),
-                onPressed: () {},
-              ),
-            ],
-          )
-        ],
-      ),
-    ),
-  );
-  }
-}
+
 
 
---
+++ lib/screens/visit_schedule_page.dart
@@ -0,0 +1,328 @@
+import'package:flutter/material.dart';
+
+class VisitSchedulePage extends StatelessWidget{
+  const VisitSchedulePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(toolbarHeight: 0),
+      body: Column(
+        children: [
+          _TopHeaderSection(), //top header/control section
+          Expanded(child: Row(
+           children: [
+             Flexible(flex: 2,
+             child: _PatientListSection(),//left panel is for patient list
+             ),
+             Flexible(
+               flex: 3,
+               child: _CalenderAndVisitDetailsSection(),//right panel is for calender
+             ),
+           ],
+          )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _TopHeaderSection extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text('Visit Schedule',
+              style: TextStyle(fontSize: 18,
+              fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              Row(
+                children: [
+                  ElevatedButton.icon(
+                  label: Text('Schedule Visit'),
+                      icon:Icon(Icons.calendar_today),
+                  onPressed: () {},
+                  ),
+                  SizedBox(width: 8),
+                  OutlinedButton(onPressed: () {},
+                      child: Text('Reschedule'),
+                  ),
+                  SizedBox(width: 8),
+                  OutlinedButton(onPressed: () {},
+                      child: Text('Cancel'),
+                  ),
+                  SizedBox(width:8),
+                  DropdownButton(
+                      value:'Options',
+                      items: const [
+                        DropdownMenuItem(value: 'Options',child: Text('Options'),
+                        ),
+                      ],
+                      onChanged: (value){},
+                  )
+                ],
+              )
+            ],
+          ),
+      SizedBox(height: 16),
+      Row(
+        children: [
+          Expanded(child: DropdownButtonFormField<String>(
+          decoration: InputDecoration(
+          labelText: 'Filters',
+        border: OutlineInputBorder(),
+      ),
+      value: 'Filters',
+      items: const[
+        DropdownMenuItem(value: 'Filters',child: Text('Filters'),
+        ),
+      ],
+      onChanged: (value) {},
+         ),
+          ),
+          SizedBox(width: 8),
+          Expanded(child: DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              labelText:'Date Range',
+              border: OutlineInputBorder(),
+            ),
+            value: 'Date Range',
+            items: const[
+              DropdownMenuItem(value: 'Date Range',child: Text('Date Range'),
+              ),
+            ],
+            onChanged: (value) {},
+          ),
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            flex: 2,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search patient or visit...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          )
+        ],
+      )
+     ],
+    ),
+    );
+  }
+}
+
+class _PatientListSection extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('April 2024',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildPatientVisitCard('Anna Davis'),
+                  SizedBox(height: 8),
+                  _buildPatientVisitCard('Walter Reed', isSelected: true),
+                  SizedBox(height: 8),
+                  _buildPatientVisitCard('Fred Somme'),
+                  SizedBox(height: 8),
+                  _buildPatientVisitCard('Sara grids'),
+                ],
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+}
+
+_buildPatientVisitCard(String name, {bool isSelected = false}) {
+  return Container(
+    decoration: BoxDecoration(
+      border: Border.all(
+        color: isSelected ? Colors.blue : Colors.grey
+      ),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    padding: const EdgeInsets.all(16),
+    child: Row(
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundImage: AssetImage('assets/avatar.png'), //will replace with an actual image. create assets folder
+        ),
+        SizedBox(width: 16),
+        Text(name),
+      ],
+    ),
+  );
+}
+
+class _CalenderAndVisitDetailsSection extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          _buildCalenderSection(),
+          SizedBox(height: 16),
+          Text('Visit Details'),
+          Expanded(
+              child: Row(
+               children: [
+                 Expanded(child: Card(
+                   child: Padding(
+                     padding: const EdgeInsets.all(16),
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Text(
+                           'Patient Health Information',
+                           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                         ),
+                         SizedBox(height: 16),
+                         _buildDetailInfoRow('Conditions'),
+                         SizedBox(height: 8),
+                         _buildDetailInfoRow('Allergies'),
+                         SizedBox(height: 8),
+                         _buildDetailInfoRow('Medications'),
+                         SizedBox(height: 8),
+                         _buildDetailInfoRow('Key History'),
+                       ],
+                     ),
+                   ),
+                 ),
+                 ),
+                 SizedBox(width: 16),
+                 Expanded(child: Card(
+                   child: Padding(
+                       padding: const EdgeInsets.all(16),
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Text('Visit Specific Notes and Actions',
+                           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                         ),
+                         SizedBox(height: 16),
+                         _buildDetailInfoRow('Required Actions'),
+                         SizedBox(height: 16),
+                         Text('Add New Notes:'),
+                         SizedBox(height: 8),
+                         Expanded(child: TextField(
+                           minLines: 5,
+                           maxLines: null,
+                           decoration: InputDecoration(
+                             border: OutlineInputBorder(),
+                           ),
+                         ),
+                         ),
+                         SizedBox(height: 16),
+                         Align(
+                           alignment: Alignment.bottomRight,
+                           child: ElevatedButton(
+                               onPressed:() {},
+                               child: Text('Update Notes'),
+                           ),
+                         ),
+                       ],
+                     ),
+                   ),
+                 ),
+                 ),
+               ],
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+_buildDetailInfoRow(String title) {
+  return Row(
+    children: [
+      Text(
+        title,
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 16),
+      Text(
+        'Sample Info Sample Info Sample Info Sample Info Sample Info Sample Info',//Replace with actual data
+        style: TextStyle(fontSize: 16),
+      ),
+    ],
+  );
+}
+_buildCalenderSection() {
+  return Column(
+    children: [
+      Row(
+        children: [
+          IconButton(
+              icon: Icon(Icons.chevron_left),
+            onPressed: () {},
+          ),
+          Text(
+            'April 2024',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          IconButton(
+              icon: Icon(Icons.chevron_right),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      SizedBox(height: 16),
+      Row(
+        children: [
+          Text('Sun', style: TextStyle(fontSize: 16)),
+          SizedBox(width: 32),
+          Text('Mon', style: TextStyle(fontSize: 16)),
+          SizedBox(width: 32),
+          Text('Tue', style: TextStyle(fontSize: 16)),
+          SizedBox(width: 32),
+          Text('Wed', style: TextStyle(fontSize: 16)),
+          SizedBox(width: 32),
+          Text('Thu', style: TextStyle(fontSize: 16)),
+          SizedBox(width: 32),
+          Text('Fri', style: TextStyle(fontSize: 16)),
+          SizedBox(width: 32),
+          Text('Sat', style: TextStyle(fontSize: 16)),
+        ],
+      ),
+      SizedBox(height: 16),
+      GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 7,),
+        itemCount: 31, //assuming 31 days in a month
+        itemBuilder: (context, index){
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Center(
+              child: Text(
+                  (index + 1).toString(),
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          );
+        },
+      ),
+    ],
+  );
+}
