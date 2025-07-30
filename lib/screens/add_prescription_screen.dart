56fe5ca47c3710b22b3f9bfb02bf5f9348b9ffa1
Rahmat10-Namayanja <namayanjarahmat@gmail.com>
2025-07-30 12:03:48 +0300

    Update add_prescription_screen.dart

--- lib/screens/add_prescription_screen.dart
+++ lib/screens/add_prescription_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
+import 'package:care_flow/models/prescription.dart'; // Correctly import the Prescription model
 class AddPrescriptionScreen extends StatefulWidget {
   final String patientId;
   final String patientName;
